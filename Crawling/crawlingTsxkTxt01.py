2ec82862a9f93015b876815599eb7399902e7170
Heisenberg <2411851860@qq.com>
2023-10-06 14:55:19 +0800

    cancel warn

--- Crawling/crawlingTsxkTxt01.py
+++ Crawling/crawlingTsxkTxt01.py
@@ -7,7 +7,8 @@ from bs4 import BeautifulSoup
 
 def request_website(url):
     headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36'}
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 '
+                      '(KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36'}
     response = requests.get(url, headers=headers)
     if response.status_code == 200:
         response.encoding = 'utf-8'
