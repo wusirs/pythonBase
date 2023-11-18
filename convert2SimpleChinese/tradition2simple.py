f737fdec4d10980d9d67765677802eb99899012e
wusirs <2411851860@qq.com>
2023-11-18 14:11:04 +0800

    繁体转简体中文

--- convert2SimpleChinese/tradition2simple.py
+++ convert2SimpleChinese/tradition2simple.py
@@ -1,6 +1,9 @@
 import os
+from io import BufferedReader
+
 import chardet
 import opencc
+import traceback
 
 '''
     繁体转简体
@@ -8,27 +11,30 @@ import opencc
 
 t2s = opencc.OpenCC('t2s')
 
-def traditional_to_simple(source_path: str, destination_path: str):
-    file_ls = os.listdir(source)
+
+def traditional_to_simple(source_path: str, destination_path: str) -> None:
+    file_ls: [str] = os.listdir(source)
     for file_name in file_ls:
-        file = source_path + '\\' + file_name
-        preview = open(file, 'rb')
-        encoding_format = chardet.detect(preview.read())['encoding']
-        # file_real_name = os.path.splitext(file)[0]
-        file_extension = os.path.splitext(file)[1]
-        print(file_extension)
+        file_absolutely_path: str = source_path + '\\' + file_name
+        preview: BufferedReader = open(file_absolutely_path, 'rb')
+        encoding_format: str = chardet.detect(preview.read())['encoding']
+        # file_real_name = os.path.splitext(file_absolutely_path)[0]
+        file_extension: str = os.path.splitext(file_absolutely_path)[1]
         try:
-            with open(file, 'r', encoding=encoding_format) as f:
-                content = f.read()
-                simplified_text = t2s.convert(content)
-                traget_file_name = file_name.replace(file_extension, '.simple' + file_extension)
-                with open(destination_path + '//' + traget_file_name, "w",
-                          encoding=encoding_format) as file:
-                    file.write(simplified_text)
-                    file.close()
-        except Exception:
-            print("%s convert fail".format(file))
+            with open(file_absolutely_path, 'r', encoding=encoding_format) as f:
+                content: str = f.read()
+                simplified_text: str = t2s.convert(content)
+                target_file_name: str = file_name.replace(file_extension, '.simple' + file_extension)
+                with open(destination_path + '\\' + target_file_name, "w",
+                          encoding=encoding_format) as file_absolutely_path:
+                    file_absolutely_path.write(simplified_text)
+                    file_absolutely_path.close()
+        except FileNotFoundError as e:
+            print("文件不存在: {0}".format(traceback.format_exc()))
             pass
+        except Exception as e:
+            print("{0} convert fail: {1}".format(file_absolutely_path, traceback.format_exc()))
+
     pass
 
 
