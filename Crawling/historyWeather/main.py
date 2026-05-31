c5d6d562eb310cbbbf5506036f2ee92fa63979b8
Bella Ciao <j2411851860@gmail.com>
2026-05-31 23:13:05 +0800

    feature(crawling): crawling history weather

---
+++ Crawling/historyWeather/main.py
@@ -0,0 +1,7 @@
+import soupHomepage
+
+if __name__ == '__main__':
+    home_page_url = ''
+    home_page_content = ''
+    home_page_path = ''
+    homepage = soupHomepage.HomePageSoup(home_page_url, home_page_content, home_page_path)
